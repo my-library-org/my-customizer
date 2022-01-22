9a8e99a46a17a674f93585c8d80982f2f7e1ccbb
david-gonzalez-coder <david-gonzalez-admin@protonmail.com>
2022-01-21 18:33:53 -0600

    first commit

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import {peerDependencies, dependencies } from './package.json'
+import {resolve} from 'path'
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'my-customizer/main.js'),
+      formats: ['es', 'cjs'],
+      fileName: (ext) => `index.${ext}.js`
+    },
+    rollupOptions: {
+      external: [...Object.keys(dependencies), ...Object.keys(peerDependencies)]
+    }
+  }
+})
